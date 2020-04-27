d94f1894c250baa87e2f0755067e7180f9e5edf5
WenkanEdwardkan <58300006+WenkanEdwardkan@users.noreply.github.com>
2020-04-27 08:58:25 +0800

    develop最终版

--- src/clock/Clock.java
+++ src/clock/Clock.java
@@ -3,6 +3,7 @@ package clock;
 public class Clock {
     private Display hour = new Display(24);
     private Display minute = new Display(60);
+
     public void star() {
         while (true) {
             minute.increase();
@@ -12,8 +13,10 @@ public class Clock {
             System.out.printf("%02d:%02d\n", hour.getValue(), minute.getValue());
         }
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         System.out.println("Hello! This is a clock:");
+        System.out.println("What a fast clock!");
         Clock clock = new Clock();
         clock.star();
     }
